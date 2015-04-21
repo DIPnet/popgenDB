cf7f554311c0eb19e70a550e856d42958435c4eb
Eric Crandall <eric.d.crandall@gmail.com>
2015-04-20 22:15:31 -0700

    Set up a config file so people can connect to their local DB files.

--- DIPnet_Stats_Script.R
+++ DIPnet_Stats_Script.R
@@ -23,25 +23,31 @@ setwd("~/Google Drive/!DIPnet_Gait_Lig_Bird/Scripts/GeneticDiversityStats")
 #Point this to keepsdata_ABGD files if you want to calculate stats by ESUs rather than by species
 #Add "_ABGD" to file name to load the ABGD assignments
 ipdb<-read.table("~/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/2015-03-24_keepsdata.txt",sep="\t",header=T,stringsAsFactors = F,quote="") 
-ipdb<-read.table("C:/Users/Chris/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/2015-03-24_keepsdata.txt", sep="\t", header=T, stringsAsFactors=F, quote="")
+
+#windows version of reading in a file
+#ipdb<-read.table("C:/Users/Chris/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/2015-03-24_keepsdata.txt", sep="\t", header=T, stringsAsFactors=F, quote="")
  
 
 #read in geographical regionalizations from Treml
-spatial<-read.csv("C:/Users/Chris/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/KD2_24Mar15_AllJoin_cln.csv", stringsAsFactors = F, na.strings=c("NA"," ",""))
+spatial<-read.csv("~/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/KD2_24Mar15_AllJoin_cln.csv", stringsAsFactors = F, na.strings=c("NA"," ",""))
+#windows version
+#spatial<-read.csv("C:/Users/Chris/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/KD2_24Mar15_AllJoin_cln.csv", stringsAsFactors = F, na.strings=c("NA"," ",""))
+
+
 #Remove commas from fn100id and fn500id (also may need to fix reunion accent issue manually)
 spatial$fn100id<-gsub(",","", spatial$fn100id)
 spatial$fn500id<-gsub(",","", spatial$fn500id)
 ipdb<-join(ipdb,spatial)
 #CHECK FOR DUPLICATES
 dups<-ipdb[duplicated(ipdb),]
-#ipdb<-ipdb[!duplicated(ipdb),]  #remove duplicates if you need to in the future
+#ipdb<-ipdb[!duplicated(ipdb),]  #remove duplicates if you need to
 
 #the following IDs should be removed if not using the ABGD dataset per Libby Liggins
 drops <- c("Acanthurus_sohal_CO1_A8","Acanthurus_sohal_CO1_RS3407","Acanthurus_sohal_CO1_RS3406","Linckia_laevigata_CO1_2130.14","Linckia_laevigata_CO1_1195.01","Linckia_laevigata_CO1_1195.02","Labroides_dimidiatus_CR_LB10","Labroides_dimidiatus_CR_LB01","Labroides_dimidiatus_CR_LB02","Labroides_dimidiatus_CR_LB09","Labroides_dimidiatus_CR_LP03","Labroides_dimidiatus_CR_LP01","Labroides_dimidiatus_CR_LP04","Labroides_dimidiatus_CR_LP02","Katsuwonus_pelamis_CR_3637_01","Katsuwonus_pelamis_CR_3637_1","Periclimenes_soror_CO1_2227.07","Periclimenes_soror_CO1_2227.01","Periclimenes_soror_CO1_2227.02","Periclimenes_soror_CO1_2227.11","Periclimenes_soror_CO1_2227.12","Periclimenes_soror_CO1_2227.1","Periclimenes_soror_CO1_2227.16","Lutjanus_kasmira_CYB_Lka_OK4", "Lutjanus_kasmira_CYB_Lka_OK3")
 ipdb<-ipdb[ipdb$IPDB_ID %in% drops == FALSE, ] 
 
 #Also found some duplicate MSIDs in the COTS data removed thusly (apparently Toonen and Worheide contributed the same data)
-cots_dups<-rownames(sp[which(duplicated(sp$materialSampleID,fromLast=F)),1:7])
+#cots_dups<-rownames(sp[which(duplicated(sp$materialSampleID,fromLast=F)),1:7])
 cots_dups<-c("1536","1539","1541","1542","1543","1546","1548","1550","1551","490","477","478","479","480","481","482","483","484","485")
 ipdb<-ipdb[rownames(ipdb) %in% cots_dups==FALSE,]
 
@@ -107,7 +113,7 @@ write.stats(diffstats,filename="./For_NESCent/DIPnet_structure_ecoregions_PhiST_
 for(r in c("sample","meow_ecoregion", "meow_prov_name", "meow_rlm_name","fn100id", "fn500id","EEZ_country")){
   divstats<-genetic.diversity.mtDNA.db(ipdb=ipdb, minseqs = 5, minsamps= 3, mintotalseqs= 0, regionalization = r)
   dir.create(file.path("./For_NESCent",r))
-  save(divstats,file=file.path("./For_NESCent",r,paste("DIPnet_stats_032415_ABGD_",r,".Rdata",sep="")))
+  save(divstats,file=file.path("./For_NESCent",r,paste("DIPnet_stats_032415_",r,".Rdata",sep="")))
   write.stats(divstats,filename=file.path("./For_NESCent",r,paste("DIPnet_stats_032415_",r,".csv",sep="")),structure=F) # for an excel-readable csv. Ignore warnings. Note this function will not overwrite, it will append to existing files
   
 }
---
+++ config_sample.R
@@ -0,0 +1,9 @@
+#This is a sample config file. Change the paths to reflect local values on your computer. Rename to "config.ini"
+
+ipdb_path<-"~/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/2015-03-24_keepsdata.txt"
+#for windows: "C:/Users/Chris/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/2015-03-24_keepsdata.txt"
+
+spatial_path<-"~/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/KD2_24Mar15_AllJoin_cln.csv"
+#for windows: "C:/Users/Chris/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/KD2_24Mar15_AllJoin_cln.csv"
+
+working_directory<-"~/Google Drive/!DIPnet_Gait_Lig_Bird/Scripts/GeneticDiversityStats"
