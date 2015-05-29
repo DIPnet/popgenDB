37b23caf4ef14fb3fbc3b0f3707d5e23546065b5
Eric Crandall <eric.d.crandall@gmail.com>
2015-05-29 16:11:12 -0700

    Restore the config_example.R file

---
+++ config_example.R
@@ -0,0 +1,10 @@
+#This is an example config file. Change the paths to reflect local values on your computer. Rename to "config.R". 
+#Once renamed it will remain unchanged during development because it is included in .gitignore
+
+ipdb_path<-"~/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/2015-03-24_keepsdata.txt"
+#for windows: "C:/Users/Chris/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/2015-03-24_keepsdata.txt"
+
+spatial_path<-"~/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/KD2_24Mar15_AllJoin_cln.csv"
+#for windows: "C:/Users/Chris/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/KD2_24Mar15_AllJoin_cln.csv"
+
+working_directory<-"~/Dropbox/r-scripts/popgenDB"
