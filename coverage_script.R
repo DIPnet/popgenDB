0c8f6d2977c72df51dc7701769a2e27ffad67bb8
Eric Crandall <eric.d.crandall@gmail.com>
2015-06-08 12:55:56 -0700

    Commit for a new branch on coverage

--- coverage_script.R
+++ coverage_script.R
@@ -15,7 +15,7 @@ library(iNEXT)
 
 #set the working directory and source stuff
 
-source("coverage_function.R")
+source("coverage_function_40percentSCtrigger.R")
 source("config.R")
 
 setwd(working_directory)
@@ -28,48 +28,68 @@ setwd(working_directory)
 #Add "_ABGD" to file name to load the ABGD assignments
 ipdb<-read.table(ipdb_path,sep="\t",header=T,stringsAsFactors = F,quote="") 
 
-#windows version of reading in a file
-#ipdb<-read.table("C:/Users/Chris/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/2015-03-24_keepsdata.txt", sep="\t", header=T, stringsAsFactors=F, quote="")
-
+#read in ABGD groups
+abgd<-read.table(abgd_path, header=T, sep="\t", stringsAsFactors = F)
 
 #read in geographical regionalizations from Treml
-spatial<-read.csv(spatial_path, stringsAsFactors = F, na.strings=c("NA"," ",""))
-#spatial<-read.csv("~/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/KD2_24Mar15_AllJoin_cln.csv", stringsAsFactors = F, na.strings=c("NA"," ",""))
-#windows version
-#spatial<-read.csv("C:/Users/Chris/Google Drive/!DIPnet_DataQC/Reunite_metadata_and_alignments/2015-03-24/KD2_24Mar15_AllJoin_cln.csv", stringsAsFactors = F, na.strings=c("NA"," ",""))
+spatial<-read.table(spatial_path, header=T, sep="\t",stringsAsFactors = F, na.strings=c("NA"," ",""), quote="")
 
+#join spatial
+ipdb<-join(ipdb,spatial, by = "IPDB_ID",type = "left")
 
-#Remove commas from fn100id and fn500id (also may need to fix reunion accent issue manually)
-spatial$fn100id<-gsub(",","", spatial$fn100id)
-spatial$fn500id<-gsub(",","", spatial$fn500id)
-ipdb<-join(ipdb,spatial, by = "IPDB_ID")
-#CHECK FOR DUPLICATES
-dups<-ipdb[duplicated(ipdb),]
-#ipdb<-ipdb[!duplicated(ipdb),]  #remove duplicates if you need to
+#join ABGD
+ipdb<-join(ipdb,abgd[,c(1,3)], by = "IPDB_ID",type = "left")
 
-#the following IDs should be removed if not using the ABGD dataset per Libby Liggins
+#the following 13 IDs should be removed if not using the ABGD dataset per Libby Liggins. They are highly divergent individuals, likely to be due to hybridization, cryptic species etc.
 drops <- c("Acanthurus_sohal_CO1_A8","Acanthurus_sohal_CO1_RS3407","Acanthurus_sohal_CO1_RS3406","Linckia_laevigata_CO1_2130.14","Linckia_laevigata_CO1_1195.01","Linckia_laevigata_CO1_1195.02","Labroides_dimidiatus_CR_LB10","Labroides_dimidiatus_CR_LB01","Labroides_dimidiatus_CR_LB02","Labroides_dimidiatus_CR_LB09","Labroides_dimidiatus_CR_LP03","Labroides_dimidiatus_CR_LP01","Labroides_dimidiatus_CR_LP04","Labroides_dimidiatus_CR_LP02","Katsuwonus_pelamis_CR_3637_01","Katsuwonus_pelamis_CR_3637_1","Periclimenes_soror_CO1_2227.07","Periclimenes_soror_CO1_2227.01","Periclimenes_soror_CO1_2227.02","Periclimenes_soror_CO1_2227.11","Periclimenes_soror_CO1_2227.12","Periclimenes_soror_CO1_2227.1","Periclimenes_soror_CO1_2227.16","Lutjanus_kasmira_CYB_Lka_OK4", "Lutjanus_kasmira_CYB_Lka_OK3")
 ipdb<-ipdb[ipdb$IPDB_ID %in% drops == FALSE, ] 
 
-#Also found some duplicate MSIDs in the COTS data removed thusly (apparently Toonen and Worheide contributed the same data)
-#cots_dups<-rownames(sp[which(duplicated(sp$materialSampleID,fromLast=F)),1:7])
-cots_dups<-c("1536","1539","1541","1542","1543","1546","1548","1550","1551","490","477","478","479","480","481","482","483","484","485")
-ipdb<-ipdb[rownames(ipdb) %in% cots_dups==FALSE,]
-
-#CHECK FOR MISSING LOCALITIES
-#note there are 1162 individual sequences with no locality! Distributed thusly:
-table(ipdb$principalInvestigator[which(is.na(ipdb$locality))])
-ipdb$locality[which(is.na(ipdb$locality))]<-"no_name"  #give "no_name" to NA localities
+# #Remove commas from fn100id and fn500id (also may need to fix reunion accent issue manually)
+# spatial$fn100id<-gsub(",","", spatial$fn100id)
+# spatial$fn500id<-gsub(",","", spatial$fn500id)
+# ipdb<-join(ipdb,spatial, by = "IPDB_ID")
+# #CHECK FOR DUPLICATES
+# dups<-ipdb[duplicated(ipdb),]
+# #ipdb<-ipdb[!duplicated(ipdb),]  #remove duplicates if you need to
+# 
+# #the following IDs should be removed if not using the ABGD dataset per Libby Liggins
+# drops <- c("Acanthurus_sohal_CO1_A8","Acanthurus_sohal_CO1_RS3407","Acanthurus_sohal_CO1_RS3406","Linckia_laevigata_CO1_2130.14","Linckia_laevigata_CO1_1195.01","Linckia_laevigata_CO1_1195.02","Labroides_dimidiatus_CR_LB10","Labroides_dimidiatus_CR_LB01","Labroides_dimidiatus_CR_LB02","Labroides_dimidiatus_CR_LB09","Labroides_dimidiatus_CR_LP03","Labroides_dimidiatus_CR_LP01","Labroides_dimidiatus_CR_LP04","Labroides_dimidiatus_CR_LP02","Katsuwonus_pelamis_CR_3637_01","Katsuwonus_pelamis_CR_3637_1","Periclimenes_soror_CO1_2227.07","Periclimenes_soror_CO1_2227.01","Periclimenes_soror_CO1_2227.02","Periclimenes_soror_CO1_2227.11","Periclimenes_soror_CO1_2227.12","Periclimenes_soror_CO1_2227.1","Periclimenes_soror_CO1_2227.16","Lutjanus_kasmira_CYB_Lka_OK4", "Lutjanus_kasmira_CYB_Lka_OK3")
+# ipdb<-ipdb[ipdb$IPDB_ID %in% drops == FALSE, ] 
+# 
+# #Also found some duplicate MSIDs in the COTS data removed thusly (apparently Toonen and Worheide contributed the same data)
+# #cots_dups<-rownames(sp[which(duplicated(sp$materialSampleID,fromLast=F)),1:7])
+# cots_dups<-c("1536","1539","1541","1542","1543","1546","1548","1550","1551","490","477","478","479","480","481","482","483","484","485")
+# ipdb<-ipdb[rownames(ipdb) %in% cots_dups==FALSE,]
+# 
+# #CHECK FOR MISSING LOCALITIES
+# #note there are 1162 individual sequences with no locality! Distributed thusly:
+# table(ipdb$principalInvestigator[which(is.na(ipdb$locality))])
+# ipdb$locality[which(is.na(ipdb$locality))]<-"no_name"  #give "no_name" to NA localities
 
 ####RUN IT###
+#source("coverage_function_UniqueHaplotypesTrigger.R")  #any gsl with pops of only unique haplotypes gets resampled by tranvservsion defined haplotypes
+source("coverage_function_40percentSCtrigger.R")  #any gsl with pops with Standardized Coverage < 0.4 gets resampled by tranvservsion defined haplotypes
+
+
+## minsamps = 6 seems reasonable based on coverage sensitivity analyses below ##
+coverage<-coverage.db(ipdb=ipdb, minseqs = 6, minsamps= 3, mintotalseqs= 0, regionalization = "ECOREGION")
+
+
+coverage_stats<-rbind(coverage_stats, coverage)
+
+
+
+
+
+
+
+## CHECKING EFFECTS OF COVERAGE SENSITIVITY ##
 ###Coverage Sensitivity###
 #Computes coverage and sample size depending on minsamp cut off
 # minseqs = minimum sequences per sampled population, 
 # minsamps = minimum sampled populations per species (after pops with n < minseqs have been removed)
 # mintotalseqs = minimum sampled sequences per species (after pops with n < minseqs have been removed)
 
-source("coverage_function_UniqueHaplotypesTrigger.R")  #any gsl with pops of only unique haplotypes gets resampled by tranvservsion defined haplotypes
-source("coverage_function_40percentSCtrigger.R")  #any gsl with pops with Standardized Coverage < 0.4 gets resampled by tranvservsion defined haplotypes
 
 coverage_stats<-data.frame()
 min_minseqs<-5
