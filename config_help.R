22744cbff9a9f104157a9212b5591a01addc4cf9
liblig <libbyliggins@gmail.com>
2015-07-17 10:58:22 +1200

    Sample config file to help you set up

---
+++ config_help.R
@@ -0,0 +1,13 @@
+#This is a sample config file. Change the paths to reflect local values on your computer 
+#(although you might want to consider this setup with github directory off your home directory) 
+#Rename this file to "config.R". 
+#It will remain unchanged during development because it is included in .gitignore
+#Windows paths look like this: "C:/Users/Chris/github/IPDB/ipdb.txt". They may or may not use tilde expansion as below.
+
+ipdb_path<-"~/github/IPDB/ipdb.txt"
+
+spatial_path<-"~/github/IPDB/ipdb_spatial.txt"
+
+abgd_path<-"~/github/IPDB/ABGD_group_assignment_JCdistance_defaults.txt"
+
+working_directory<-"~/github/popgenDB/output"
